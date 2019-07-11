b641d9202b103209b5542275d8ab3719b6bfb624
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2019-07-10 21:32:00 -0700

    clean build script

--- build.sh
+++ build.sh
@@ -1,13 +1,26 @@
 #!/bin/bash -ex
 
-# PKG_NAME="ungoogled-chromium"
-CONCURRENCY="$(nproc)"
 OUT_DIR="/out"
+if [ -z "$CONCURRENCY" ]
+then
+    CONCURRENCY="$(nproc)"
+fi
 
-wget "https://aur.archlinux.org/cgit/aur.git/snapshot/${PKG_NAME}.tar.gz"
-tar zxvf "${PKG_NAME}.tar.gz"
+if ! touch /out/test_write 2>/dev/null
+then
+    echo "Cannot write to output directory, will not proceedstopping early." >&2
+    exit 1
+fi
+
+rm /out/test_write
+
+TARBALL="${PKG_NAME}.tar.gz"
+TARBALL_URL="https://aur.archlinux.org/cgit/aur.git/snapshot/${TARBALL}"
+
+wget "$TARBALL_URL"
+tar zxvf "$TARBALL"
 cd "${PKG_NAME}"
-sed -i -E "s/^(\s*)([a-z])?make(\s+|$)/\1\2make -j${CONCURRENCY}\3/" PKGBUILD
+sed -i -E "s/^(\s*)make(\s+|$)/\1make -j${CONCURRENCY}\2/" PKGBUILD
 
 makepkg --noconfirm -s
 cp ./*.tar.xz "$OUT_DIR/"
